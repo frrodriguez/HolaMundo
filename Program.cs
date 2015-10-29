f6177ab7d21b40668cc84b0ffb561c3f6403b124
baltasarq <baltasarq@gmail.com>
2015-10-29 13:36:16 +0100

    Otrora nuevo

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 
 namespace HolaMundo {
 	class MainClass {
 		public static void Main(string[] args)
 		{
-			string msg = "Hola, nuevo nuevo mundo!";
+			string msg = "Hola, otrora nuevo mundo!";
 			Console.Write( msg + "\n" );
 		}
 	}
