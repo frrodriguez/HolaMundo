8aad5fd3982d4ec220466b0c27d6aa2d85c398fc
Baltasar García Perez-Schofield <baltasarq@gmail.com>
2015-10-29 13:35:43 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace HolaMundo {
 	class MainClass {
 		public static void Main(string[] args)
 		{
-			string msg = "Hola, nuevo mundo!";
+			string msg = "Hola, nuevo nuevo mundo!";
 			Console.Write( msg + "\n" );
 		}
 	}
