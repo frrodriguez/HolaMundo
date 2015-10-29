d2c20007a57b86c504cd828e10bc639ee2774dba
baltasarq <baltasarq@gmail.com>
2015-10-29 13:11:57 +0100

    Nuevo mensaje

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace HolaMundo {
 	class MainClass {
 		public static void Main(string[] args)
 		{
-			Console.WriteLine( "Hola, mundo!" );
+			Console.WriteLine( "Hola, nuevo mundo!" );
 		}
 	}
 }
