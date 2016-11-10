e8f3258fbcb6e45bf67483d030c99828b45f03ca
baltasarq <baltasarq@gmail.com>
2016-11-10 13:29:27 +0100

    Otro cambio de mensaje

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace HolaMundo {
 	class MainClass {
-		public const string Mensaje = "Hola, mundo!";
+		public const string Mensaje = "Hola, nuevo mundo!";
 
 		public static void Main(string[] args)
 		{
